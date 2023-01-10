583e3e3ddf75bd3acb9b098866ad58b9cefaac7b
Breno Rocha <51891048+Bre323@users.noreply.github.com>
2023-01-10 20:34:19 -0300

    Update Main.java

--- LabelandPanel/Main.java
+++ LabelandPanel/Main.java
@@ -1,21 +1,37 @@
 package LabelandPanel;
 
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.border.Border;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		//JLabel = RÛtulos que podem ser exibidos nos frames
+		//JLabel = R√≥tulos que podem ser exibidos nos frames
 		
-		JLabel label = new JLabel();  //CriaÁ„o de um JLabel
-		label.setText("Apenas um texto aleatÛrio!");  //Insere texto no label criado. TambÈm pode ser colocado logo na declaraÁ„o
+		Border border = BorderFactory.createLineBorder(Color.blue, 3);
+		
+		JLabel label = new JLabel();  //Cria√ß√£o de um JLabel
+		label.setText("Apenas um texto aleat√≥rio!");  //Insere texto no label criado. Tamb√©m pode ser colocado logo na declara√ß√£o
+		label.setHorizontalAlignment(JLabel.CENTER);  //Define o posicionamento do label de acordo com o eixo X
+		label.setVerticalAlignment(JLabel.CENTER);    //Define o posicionamento do label de acordo com o eixo Y
+		label.setForeground(Color.blue);  //Define a cor da fonte
+		label.setFont(new Font("Arial", Font.BOLD, 20));  //Define outras caracter√≠sticas da fonte, como tipo e tamanho
+		label.setBackground(Color.black);  //Define a cor de fundo
+		label.setOpaque(true);  //Mostra a cor de fundo definida pelo setBackground
+		label.setBorder(border);  //Coloca a borda no label
+		label.setBounds(95, 75, 300, 300);  //Define a posi√ß√£o e o dimensionamento do label dentro do frame
 		
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(500,500);
+		frame.setSize(500, 500);
 		frame.setVisible(true);
+		frame.setResizable(false);
+		frame.setLayout(null);
 		frame.add(label);
 
 	}
