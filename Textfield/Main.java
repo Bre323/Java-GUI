340e6e40ffcc95c166d86c6cbc707188f9ef512c
Breno Rocha <51891048+Bre323@users.noreply.github.com>
2023-01-13 10:21:38 -0300

    Add files via upload

---
+++ Textfield/Main.java
@@ -0,0 +1,13 @@
+package Textfield;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		//JTextField é um campo de texto que pode ser usado para a inserção de dados pelo teclado
+		
+		new MyFrame();
+		
+	}
+
+}
